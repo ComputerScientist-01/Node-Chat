152de07d794da243cdf6bc9cc4ecba911270a60a
ComputerScientist-01 <rathi406@gmail.com>
2022-01-24 15:40:51 +0530

    production : set cors origin to true

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // connect to socket io
-const io = require("socket.io")(3000);
+const io = require("socket.io")(3000, {
+	cors: {
+		origin: true,
+	},
+});
 
 // send hello world message on connection
 io.on("connection", (socket) => {
