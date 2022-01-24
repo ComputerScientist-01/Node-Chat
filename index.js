0585dc03990f7889dec807c6766eafb3c4bd8c2d
ComputerScientist-01 <rathi406@gmail.com>
2022-01-24 16:50:02 +0530

    dev : broadcast message

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ const io = require("socket.io")(3000, {
 
 // send hello world message on connection
 io.on("connection", (socket) => {
-	socket.emit("chat-message", "hello world");
+	socket.on("send-chat-message", (message) => {
+		socket.broadcast.emit("chat-message", message);
+	});
 });
