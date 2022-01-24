e687edb972cf27ab76685fff5411259657b31711
ComputerScientist-01 <rathi406@gmail.com>
2022-01-24 15:03:33 +0530

    docs : add comments

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// connect to socket io
 const io = require("socket.io")(3000);
 
+// send hello world message on connection
 io.on("connection", (socket) => {
 	socket.emit("chat-message", "hello world");
 });
