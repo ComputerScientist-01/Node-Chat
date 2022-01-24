2d05e207a0d861ac59370804dedc532528eac3e8
ComputerScientist-01 <rathi406@gmail.com>
2022-01-24 16:43:32 +0530

    dev: implement client message handling

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const socket = io("http://localhost:3000");
+// get the message form from the send container
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
 // Listening for a message from the server.
 // and logging it to console
 socket.on("chat-message", (data) => {
 	console.log(data);
+});
+
+// When the user clicks the submit button, the message is sent to the server.
+messageForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const message = messageInput.value;
+	socket.emit("send-chat-message", message);
+	messageInput.value = "";
 });
