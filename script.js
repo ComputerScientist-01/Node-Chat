bcd30e5aa35e9be243559370ceff96990c2760d5
ComputerScientist-01 <rathi406@gmail.com>
2022-01-26 17:45:39 +0530

    dev : append msg to browser

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const socket = io("http://localhost:3000");
 // get the message form from the send container
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
-
+const messageContainer = document.getElementById("message-container");
 // Listening for a message from the server.
 // and logging it to console
 socket.on("chat-message", (data) => {
-	console.log(data);
+	appendMessage(data);
 });
 
 // When the user clicks the submit button, the message is sent to the server.
@@ -16,3 +16,9 @@ messageForm.addEventListener("submit", (e) => {
 	socket.emit("send-chat-message", message);
 	messageInput.value = "";
 });
+
+function appendMessage(message) {
+	const messageElement = document.createElement("div");
+	messageElement.innerText = message;
+	messageContainer.append(messageElement);
+}
